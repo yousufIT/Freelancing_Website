beb1c97abdd0d40c2374bb654e2b1c0ceb4a2e62
Abdullah Masto <abdullahmasto77@gmail.com>
2024-10-13 11:53:23 +0300

    change the style to be like the main style of project components

--- code-shpere/src/app/app.routes.ts
+++ code-shpere/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectListComponent } from './components/project/project-list/project-list.component';
 import { ProjectDetailsComponent } from './components/project/project-details/project-details.component';
-import { CreateProjectComponent } from './create-project/create-project.component';
 import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
 import { ReviewComponent } from './review/review.component';
 import { AuthGuard } from './guards/auth.guard';
@@ -35,7 +34,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/projects', pathMatch: 'full' },
   { path: 'projects', component: ProjectListComponent },
   { path: 'project/:id', component: ProjectDetailsComponent },
-  { path: 'create-project', component: CreateProjectComponent, canActivate: [AuthGuard] },
   { path: 'portfolio', component: PortfolioItemComponent },
   { path: 'reviews', component: ReviewComponent },
   { path: 'projects/:projectId/bids/create', component: BidCreateComponent },
